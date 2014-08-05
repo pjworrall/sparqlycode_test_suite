57365046b0a9e3969d151f63affb1e9b529a284c
Paul Worrall <paul.worrall@interition.net>
2014-08-05 21:09:16 +0100

    another test class to try and reproduce a bug

---
+++ src/main/java/net/interition/sparqlycode/testsuite/OverloadedMethodsBugReproducer.java
@@ -0,0 +1,34 @@
+package net.interition.sparqlycode.testsuite;
+
+public class OverloadedMethodsBugReproducer {
+	
+	private String jobId;
+	private Number payload;
+	private long delay;
+	private String cronEntry;
+	private long start;
+	private long period;
+	private int repeat;
+	
+    public void schedule(String jobId, Number payload, long delay) throws Exception {
+        this.jobId = jobId;
+        this.payload = payload;
+        this.delay=delay;
+    }
+
+    public void schedule(String jobId, Number payload, String cronEntry, long start, long period, int repeat) throws Exception {
+        this.jobId = jobId;
+        this.payload = payload;
+        this.cronEntry = cronEntry;
+        this.start = start;
+        this.period = period;
+        this.repeat = repeat;
+    }
+
+    public void schedule(String jobId, Number payload, String cronEntry) throws Exception {
+        this.jobId = jobId;
+        this.payload = payload;
+        this.cronEntry = cronEntry;
+    }
+
+}
