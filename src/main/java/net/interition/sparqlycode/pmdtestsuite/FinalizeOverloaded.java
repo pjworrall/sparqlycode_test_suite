7f65ac98f89e48b3982e22d4f62dc76ea3b51af5
adamnogradi <adam.nogradi@interition.net>
2014-08-11 20:08:07 +0200

    Adding a few more PMD tests

--- src/main/java/net/interition/sparqlycode/pmdtestsuite/FinalizeOverloaded.java
+++ src/main/java/net/interition/sparqlycode/pmdtestsuite/FinalizeOverloaded.java
@@ -1,6 +1,12 @@
 package net.interition.sparqlycode.pmdtestsuite;
 
+/**
+ * 
+ * @author Adam Nogradi, Interition Ltd.
+ *
+ */
 public class FinalizeOverloaded {
+	// Comment out the below method to make the test pass.
 	public void finalize (int a) {
 	}
 }
--- src/main/java/net/interition/sparqlycode/pmdtestsuite/FinalizeShouldBeProtected.java
+++ src/main/java/net/interition/sparqlycode/pmdtestsuite/FinalizeShouldBeProtected.java
@@ -1,6 +1,12 @@
 package net.interition.sparqlycode.pmdtestsuite;
-
+/**
+ * 
+ * @author Adam Nogradi, Interition Ltd.
+ *
+ */
 public class FinalizeShouldBeProtected {
+	// Change the access modifier of the below method to
+	// 'protected' to make the test pass.
 	public void finalize () {
 
 	}
