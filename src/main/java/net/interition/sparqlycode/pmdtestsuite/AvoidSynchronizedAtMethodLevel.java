ff9ab610b82a1172308ec0829c817a245e4db9e9
Adam Nogradi <adam.nogradi@interition.net>
2014-08-17 17:24:16 +0200

    A few more fixes / corrections

--- src/main/java/net/interition/sparqlycode/pmdtestsuite/AvoidSynchronizedAtMethodLevel.java
+++ src/main/java/net/interition/sparqlycode/pmdtestsuite/AvoidSynchronizedAtMethodLevel.java
@@ -2,7 +2,7 @@ package net.interition.sparqlycode.pmdtestsuite;
 
 public class AvoidSynchronizedAtMethodLevel {
 	// Remove the 'synchronized' keyword from the below method  to
-	// make the test pass.
+	// make the test fail.
 	synchronized void thisMethodShouldntBeSynchronized () {
 		// use block-level synchronization instead.
 	}
--- src/main/java/net/interition/sparqlycode/pmdtestsuite/BadCloneImplementation.java
+++ src/main/java/net/interition/sparqlycode/pmdtestsuite/BadCloneImplementation.java
@@ -1,7 +1,7 @@
 package net.interition.sparqlycode.pmdtestsuite;
 
 public class BadCloneImplementation {
-	// implement the 'Cloneable' interface to make the test pass.
+	// implement the 'Cloneable' interface to make the test fail.
 	
 	    public BadCloneImplementation clone(){
 	        return new BadCloneImplementation(); // This is bad
