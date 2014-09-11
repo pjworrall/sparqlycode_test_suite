c1f357aa97456c5d1c0b0837b6151085ecda20f6
Adam Nogradi <adam.nogradi@interition.net>
2014-09-11 23:46:47 +0200

    Adding printout to checker class to confirm TTL validity

--- src/main/java/net/interition/sparqlycode/testsuite/ValidityChecker.java
+++ src/main/java/net/interition/sparqlycode/testsuite/ValidityChecker.java
@@ -2,9 +2,6 @@ package net.interition.sparqlycode.testsuite;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-
-import org.apache.jena.riot.RiotException;
-
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 
@@ -12,5 +9,6 @@ public class ValidityChecker {
 	public static void main(String[] args) throws IOException {
 	    Model model=ModelFactory.createDefaultModel();
 	    model.read(new FileInputStream("src/test/resources/sparqlycode.ttl"),null,"TTL");
+	    System.out.println("=====================\nTTL file is valid...\n=====================");
 	}
 }
